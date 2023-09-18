039a23da5b8879213c74a99c897b4f8a4d241b9f
Riddhi-Gandhi <riddhigandhi1508@gmail.com>
2023-09-18 22:49:25 +0530

    real-estate

---
+++ project/components/footer.php
@@ -0,0 +1,27 @@
+<!-- footer section starts  -->
+
+<footer class="footer">
+
+   <section class="flex">
+
+      <div class="box">
+         <a href="home.php"><span>home</span></a>
+         <a href="contact.php"><span>contact</span></a>
+         <a href="listings.php"><span>all listings</span></a>
+         <a href="saved.php"><span>saved properties</span></a>
+      </div>
+
+      <div class="box">
+         <a href="#"><span>facebook</span><i class="fab fa-facebook-f"></i></a>
+         <a href="#"><span>twitter</span><i class="fab fa-twitter"></i></a>
+         <a href="#"><span>linkedin</span><i class="fab fa-linkedin"></i></a>
+         <a href="#"><span>instagram</span><i class="fab fa-instagram"></i></a>
+
+      </div>
+
+   </section>
+
+
+</footer>
+
+<!-- footer section ends -->
